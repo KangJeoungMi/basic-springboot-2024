33739dfaa00aa5045b898ccce8296f2615dd2a7d
JeoungMi Kang <141222777+KangJeoungMi@users.noreply.github.com>
2024-06-27 16:57:33 +0900

    Fit : 비밀번호 변경(메일 전송 REST API))

--- spring03/backboard/src/main/java/com/jeongmi/backboard/restcontroller/MailController.java
+++ spring03/backboard/src/main/java/com/jeongmi/backboard/restcontroller/MailController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -21,7 +20,7 @@ public class MailController {
 
     @PostMapping("/test-email")
     public ResponseEntity<HttpStatus> testEmail() {
-        String to = "kjm8090@naver.com";
+        String to = "kbg973855@gmail.com";
         String subject = "전송테스트 메일";
         String message = "테스트 메일 메시지 입니다";
 
