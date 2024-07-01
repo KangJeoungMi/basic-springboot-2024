bf09bdfc9c631c9688134c1c5ef81bb45ced47f2
JeoungMi Kang <141222777+KangJeoungMi@users.noreply.github.com>
2024-07-01 17:03:04 +0900

    Feat : (프론트보드)리액트 스프링부트 연동

--- react01/basic-app/src/App.js
+++ react01/basic-app/src/App.js
@@ -1,43 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import CustomButton from './component/CustomButton';
+import IncButton from './component/IncButton'
+import { useState } from 'react';
 
-// 데이터 생성 시 보통 const
+// 데이터 생성시 보통 const
 const ironMan = {
-  name : 'Trony Stark',
-  heroname: 'Iron-man',
-  imgUrl : 'https://cdn.pixabay.com/photo/2020/11/28/03/19/iron-man-5783522_640.png',
-  imgsize: 100
-}
+  name: 'Tony Stark',
+  heroName: 'Iron-man',
+  imgUrl: 'https://cdn.pixabay.com/photo/2020/11/28/03/19/iron-man-5783522_640.png',
+  imgSize: 100,
 
+}
 const weapons = [
-  { title : 'Replusor Beam', idx : 1},
-  { title : 'Unibeam Blaster', idx : 2},
-  { title : 'Smart missile', idx : 3}
+  { title: 'Replusor Beam', idx: 1 },
+  { title: 'Unibeam Blaster', idx: 2 },
+  { title: 'lazor cutter', idx: 3 }
 ];
 
-const listWeapons = weapons.map(weapon => 
+const listWeapons = weapons.map(weapon =>
   <li key={weapon.idx}>
     {weapon.title}
   </li>
-)
+);
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick(){
+    setCount(count+1);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{ironMan.heroname}</h1>
+        <h1>{ironMan.heroName}</h1>
         <img className='profile'
-              src = {ironMan.imgUrl}
-              alt={ironMan.name} 
-              style={{
-                width:ironMan.imgsize,
-                height:ironMan.imgsize,
-                borderRadius: '50%'
-              }}/>
-
-        <ul>{listWeapons}</ul>
-        <CustomButton/>
+          src={ironMan.imgUrl}
+          alt={ironMan.name + '변신사진'}
+          style={{
+            width: ironMan.imgSize,
+            height: ironMan.imgSize,
+            borderRadius: '50%'
+          }}
+        />
+        <IncButton count={count} onClick={handleClick}/>
+        <IncButton count={count} onClick={handleClick}></IncButton>
+        {/* <ul>{listWeapons}</ul>
+        <CustomButton data={ironMan} /> */}
       </header>
     </div>
   );
